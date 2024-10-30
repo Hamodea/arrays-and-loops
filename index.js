ed7a569f16865daac8f38b9672cd75549e05a81d
Mohmad&W <42297664+Hamodea@users.noreply.github.com>
2024-10-30 12:40:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,98 @@
+// svarar på uppgifterna,
+const names = ["Zlatan", "Beckham", "Zidane", "Maradona", "Pele", "Ronaldo"];
+console.log(names[3]); // svar är Mardona 
+
+// fråga 2 , lägg till namnet "Drogba" i slutet av arrayen
+const newPlayer = names.push("Drogba");
+console.log(names);
+
+// fråga 3 , Lägg nu till namnet "Figo" i börja av arrayen.
+// metoden är unshift() för att lägga element i början
+const addPlayer = names.unshift("Figo");
+console.log(names);
+
+// fråga 4 , Ta nu bort det sista namnen från array med pop().
+names.pop()
+console.log(names);
+
+// fråga 5 , flytta namnet "Ronaldo" från sluten av arrayen till början av arrayen?
+
+// första sätt 
+
+const changeIndex = names.pop();
+names.unshift("Ronaldo");
+console.log(names);
+
+// andra sätt med splice metod
+const [item] = names.splice(6, 1); // ta bort index 6 från list
+console.log(names);
+names.splice(0, 0, item); // lägga element i början 
+console.log(names);
+
+
+// fråga 6
+/*
+När vi definierar en array  med const, är det bara själva referensen till arrayen  som är konstant. 
+Det betyder att vi inte kan byta ut arrayen mot en ny array eller omdefiniera den, men vi kan fortfarande modifiera innehållet i arrayen 
+ med Destruktiva metoder eftersom själva datatypen är en referenstyp.
+*/
+
+// fråg 7 , t kontrollera om "Messi" finns i arrayen
+const kontrol = names.includes("messi"); // includes return  boolean (true eller false )
+console.log(kontrol); // svar är false
+
+
+// fråga 8 , kontrollera om zlata är i lista 
+const check = names.includes("Zlatan");
+if (check === true){
+    console.log("Zlatan är här");
+}
+else{
+    console.log("Zlatan är inte här");
+}
+
+// fråga 9 , Skriv ut indexet för "Maradona" i arrayen "players"
+
+const playerIndex = names.indexOf("Maradona");
+console.log(playerIndex);
+
+
+// fråga 10 , Använd indexOf() för att försöka hitta indexet för "Messi"
+
+const messiIndex = names.indexOf("messi");
+console.log(messiIndex); // return -1 
+
+
+// fråga 11 join metod
+
+const allPlayer = names.join(", ");
+console.log(allPlayer);
+
+// fråga 12,
+const Player = names.join("-");
+console.log(allPlayer);
+
+
+// fråga 13
+names.splice(3, 2);
+console.log(names);
+
+// fråga 14 lägga 
+const spelare = names.splice(2, 0, "Ronaldinio");
+console.log(names);
+
+// fråga 15
+const indexRonaldo = names.indexOf("Ronaldo");
+
+if (indexRonaldo !== -1) {
+    // Ta bort "Ronaldo" från arrayen om han finns där
+    names.splice(indexRonaldo, 1);
+}
+
+console.log(names); 
+
+
+
 // fråga 16, skapa en ny array från "players" som innehåller de tre första spelarna
 
 const players = ["Zlatan", "Beckham", "Zidane", "Ronaldo", "Romario", "Maradona"];
